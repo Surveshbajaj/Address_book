44ea7728a532c80a000c64f7c211744a50b367c5
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-03-07 13:17:02 +0530

    [Survesh Bajaj]- Add. Provide User Friendly Error Response in case validation fails

--- addressbook/src/main/java/com/bridgelabz/addressbook/DTO/ContactDTO.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/DTO/ContactDTO.java
@@ -1,9 +1,6 @@
 package com.bridgelabz.addressbook.DTO;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,9 @@ public class ContactDTO {
     @Size(max = 50, message = "Name must not exceed 50 characters")
     @Pattern(regexp = "^[A-Za-z ]+$", message = "Name can only contain alphabets and spaces")
     private String name;
+    
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
     private String phone;
 
